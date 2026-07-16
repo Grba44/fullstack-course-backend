460ab0ffa1b033ea37a23453edb16131fe410246
Grba44 <matijagrbicbgd@gmail.com>
2026-07-16 20:25:38 +0200

    new users route and user administration

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,12 +1,13 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
-notesRouter.get("/:id", async (request, response, next) => {
+notesRouter.get("/:id", async (request, response) => {
   const note = await Note.findById(request.params.id);
   if (note) {
     response.json(note);
@@ -15,24 +16,34 @@ notesRouter.get("/:id", async (request, response, next) => {
   }
 });
 
-notesRouter.post("/", async (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
+
+  const user = await User.findById(body.userId);
+
+  if (!user) {
+    return response.status(400).json({ error: "userId missing or not valid" });
+  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
-notesRouter.delete("/:id", async (request, response, next) => {
+notesRouter.delete("/:id", async (request, response) => {
   await Note.findByIdAndDelete(request.params.id);
   response.status(204).end();
 });
 
-notesRouter.put("/:id", (request, response, next) => {
+notesRouter.put("/:id", (request, response) => {
   const { content, important } = request.body;
 
   Note.findById(request.params.id)
